a56fe4b5b67814c739bbd2a7419a7fc7cb7ad7ee
sebasmck <corporate.att@gmail.com>
2022-01-04 15:13:04 -0600

    Renaming

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,30 @@
+pipeline {
+
+    tools {
+        gradle 'gradle-6.6.1'
+    }
+
+    agent any
+
+    stages {
+        stage('init'){
+            steps {
+                echo "Calling A.a()"
+            }
+        }
+//        stage('Build') {
+//            steps {
+//                sh '''
+//                    gradle wrapper --gradle-version 6.6.1
+//                    ./gradlew build
+//                '''
+//            }
+//        }
+//        stage('Tests'){
+//            steps{
+//                println('Running Tests')
+//                sh './gradlew test'
+//            }
+//        }
+    }
+}
